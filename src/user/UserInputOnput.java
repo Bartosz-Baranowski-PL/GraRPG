077b1faf4c4a63230f754ae0ecaf1e1e8022a825
Dawid Bielecki <dawciobiel@gmail.com>
2025-12-20 16:20:06 +0100

    Refaktoryzacja klasy UserInputOnput

--- src/user/UserInputOnput.java
+++ src/user/UserInputOnput.java
@@ -1,28 +1,29 @@
 package user;
 
-import game.Hero;
+import static java.lang.System.out;
 
-public class UserInputOnput {
-    private final Hero hero = Hero.getInstance();
+public class UserInputOutput {
 
-    public void choiseOnput(){
-        StringBuilder sb = new StringBuilder();
-        sb.append("\n -----------------------MENU----------------------- \n");
-        sb.append("\nCo teraz zamierzasz zrobić?");
-        sb.append("\n 1. Zdobyć Dungeuron!");
-        sb.append("\n 2. Szukać pracy.");
-        sb.append("\n 3. Wyprawa do szkoły aby poprawić swoje parametry.");
-        sb.append("\n 4. A może by się tak po prostu najeść? To jest to - podróż do do karczmy!");
-        sb.append("\n 5. Sprawdzić swoje statystyki oraz ekwipunek.");
-        sb.append("\n X. Inny wybór wyłączy gre\n");
-        sb.append("\n -----------------------MENU----------------------- \n");
-        sb.append("\nTwój wybór to: ");
-        System.out.println(sb);
+    public void choiceOutput() {
+        //  Tutaj jest tylko jedno wywołanie `String.out.println()` więc nie ma sensu używać StringBuilder
+        out.println(
+                "\n -----------------------MENU----------------------- \n" +
+                "\nCo teraz zamierzasz zrobić?" +
+                "\n 1. Zdobyć Dungeuron!" +
+                "\n 2. Szukać pracy." +
+                "\n 3. Wyprawa do szkoły aby poprawić swoje parametry." +
+                "\n 4. A może by się tak po prostu najeść? To jest to - podróż do karczmy!" +
+                "\n 5. Sprawdzić swoje statystyki oraz ekwipunek." +
+                "\n X. Inny wybór wyłączy grę\n" +
+                "\n -----------------------MENU----------------------- \n" +
+                "\nTwój wybór to: "
+        );
     }
 
-    public void run(){
-        StringBuilder sb = new StringBuilder("\n *___Wstęp___* \n");
-        sb.append("Jakie jest Twoje miano?");
-        System.out.println(sb);
+    public void run() {
+        out.println(
+                "\n *___Wstęp___* \n" +
+                "Jakie jest Twoje miano?"
+        );
     }
 }
